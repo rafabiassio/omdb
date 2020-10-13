e91d67e2bce020f3fa063886d419717d0f6cf410
Rafael Biassio <rafaelobiassio@gmail.com>
2020-10-13 18:50:41 -0400

    Initial config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+	entry: path.resolve(__dirname, "src", "index.js"),
+	output: {
+		path: path.resolve(__dirname, "public"),
+		filename: "bundle.js",
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, "public"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+			},
+			{
+				test: /\.(png|gif|jpe?g|woff|woff2|eot|ttf|svg)$/,
+				use: {
+					loader: "url-loader",
+				},
+			},
+		],
+	},
+};
