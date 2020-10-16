9be7039a23868cec8bf3cfca3776feb04d24f149
Rafael Biassio <rafaelobiassio@gmail.com>
2020-10-16 03:56:03 -0400

    .

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
+require('dotenv').config()
 
 app.use(express.static(__dirname + '/public'))
 
